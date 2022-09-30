c46c3d554f44c66f161934bf52a57f6c766ee613
azimabdurasulov <azimjon20050604@gmail.com>
2022-09-30 18:56:41 +0500

    20-topshiriq

--- basic_func20.py
+++ basic_func20.py
@@ -1,3 +1,10 @@
 # Create a function called main.
 # Create function arguments a and b.
 # Return the multiplication of a and b.
+import re
+
+
+def  main(a, b):
+    return a * b
+
+print(main(4, 7))
